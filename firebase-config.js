d410f9c56b29f7246f7cdac0781c19d95ad82199
mabbasaskri-create <m.abbas.askri@gmail.com>
2026-05-02 18:54:27 +0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase config - Compat SDK (works with all pages)
 const firebaseConfig = {
-  apiKey: "AIzaSyBV3y_XshRKvCWw6GPUbUxaALI5HhbhM",
+  apiKey: "AIzaSyBySV3y_XshRKvCWw6GPUbUxaALI5HhbhM",
   authDomain: "girls-fit-e5735.firebaseapp.com",
   projectId: "girls-fit-e5735",
   storageBucket: "girls-fit-e5735.firebasestorage.app",
   messagingSenderId: "1077696059801",
-  appId: "1:1077696059801:web:51778a835c80a70bb91b08",
-  measurementId: "G-5Y2BYVN7H5"
+  appId: "1:1077696059801:web:a237b96338a04408b91b08",
+  measurementId: "G-4465R1LWS9"
 };
 
 // Initialize Firebase
